23d9fde6e127efda810aa063793fec4051c5a9de
Allenwdk <15620209105@163.com>
2025-09-25 13:57:22 +0800

    modified:   src/setting/HomeSetting.ts

--- src/setting/HomeSetting.ts
+++ src/setting/HomeSetting.ts
@@ -3,7 +3,7 @@ export const mainTitle = "Allenwdk's Blog";
 //主题色装饰字
 export const mainTitleBlueDecoration = ".";
 //副标题
-export const subTitle = "A Front-End ";
+export const subTitle = "A Novice ";
 //副标题主题色装饰字
 export const subTitleBlueDecoration = "Developer";
 //打字机动画轮播数组
